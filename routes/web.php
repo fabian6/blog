78161bafb5bc952aab601f2c776ad8d13d27788a
fabian <fabian6_03@hotmail.com>
2020-06-23 00:11:06 -0700

    agrega base de datos para el blog y tabla para las publicaciones del blog

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,10 @@
 
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts= App\Post::latest('published_at')->get();
+    return view('welcome',compact('posts'));
 });
+
+// Route::get('posts',function(){
+//     return App\Post::all();
+// });
